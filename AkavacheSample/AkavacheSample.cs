b8671bd8f3c277ac4072d0bae1e6ea3b9249a36f
Hironobu OKUYAMA <okuokuoku@gmail.com>
2015-12-02 00:20:01 +0900

    Add comments

--- AkavacheSample/AkavacheSample.cs
+++ AkavacheSample/AkavacheSample.cs
@@ -11,23 +11,27 @@ namespace AkavacheSample
         public App()
         {
             var nameEntry  = new Entry { Placeholder = "名前を入力" };
-            var ageEntry   = new Entry { Placeholder = "年齢を入力" };
+            var ageEntry   = new Entry { Placeholder = "年齢を入力(数値のみ)" };
             var saveButton = new Button { Text = "保存" };
             var loadButton = new Button { Text = "読み出し" };
 
             saveButton.Clicked += async (sender, e) => 
             {
+                // Person に詰めて…
                 var person = new Person { 
                     PersonName = nameEntry.Text, 
                     PersonAge  = Convert.ToInt16(ageEntry.Text) 
                 };
 
-                await BlobCache.LocalMachine.InsertObject("person", person);
+                // 保存
+                await BlobCache.LocalMachine.InsertObject("person", person); 
             };
 
             loadButton.Clicked += async (sender, e) => 
             {
+                // Akavache で Person を読み出し
                 var loaded = await BlobCache.LocalMachine.GetObject<Person>("person");
+                // 各テキストボックスに設定
                 nameEntry.Text = loaded.PersonName;
                 ageEntry.Text  = loaded.PersonAge.ToString();
             };
